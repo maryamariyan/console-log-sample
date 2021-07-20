551f42bc1376a2f47481cd18108979b155013f07
Maryam Ariyan <maryam.ariyan@microsoft.com>
2021-07-20 11:17:31 -0700

    AddSimpleConsole to console template

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace Demo
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using ILoggerFactory loggerFactory =
+                LoggerFactory.Create(builder =>
+                    builder.AddSimpleConsole(options =>
+                    {
+                        options.IncludeScopes = true;
+                        options.SingleLine = true;
+                        options.TimestampFormat = "hh:mm:ss ";
+                    }));
+
+            ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+            using (logger.BeginScope("[scope is enabled]"))
+            {
+                logger.LogInformation("Hello World!");
+                logger.LogInformation("Logs contain timestamp and log level.");
+                logger.LogInformation("Each log message is fit in a single line.");
+            }
         }
     }
 }
